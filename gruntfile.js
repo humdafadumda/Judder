fb32c8f35b7621154fcdc78bb9703469b2f7623e
humdafadumda <huppeldepup@gmail.com>
2014-12-03 14:47:41 +0100

    Add grunfile an package info

---
+++ gruntfile.js
@@ -0,0 +1,26 @@
+﻿module.exports = function (grunt) {
+
+  grunt.initConfig({
+    emberTemplates: {
+      compile: {
+        options: {
+          templateBasePath: "public/scripts/judder/templates/",
+          templateName: function (sourceFile) {
+            return sourceFile.replace("public/scripts/judder/templates/", '');
+          }
+        },
+        files: {
+          "public/scripts/judder/templates.js": "public/scripts/judder/templates/**/**/*.hbs"
+        }
+      }
+    },
+    watch: {
+      files: "/public/scripts/judder/templates/**/**/*.hbs",
+      tasks: "emberTemplates"
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-ember-templates');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', "emberTemplates");
+};
